1dfd7bf666cc2c68410272797f6dd0caa1334215
simapohotona <mosimapohotona@gmail.com>
2025-11-03 00:09:28 +0200

    Complete Task 3: Created UserProfile component and passed data using props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,18 +1,18 @@
 
-
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 
-function App() {
-  const [count, setCount] = useState(0)
 function App() {
   return (
     <>
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
     </>
   );
 }
