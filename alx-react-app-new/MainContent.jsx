3dab49dc480f50b86a47aec86df875b36f675022
simapohotona <mosimapohotona@gmail.com>
2025-11-16 23:18:19 +0200

    Added state and dynamic rendering logic to MainContent component

--- alx-react-app-new/MainContent.jsx
+++ alx-react-app-new/MainContent.jsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
+
 function MainContent() {
+  const [count, setCount] = useState(0);
+
   return (
     <main
       style={{
         padding: "20px",
         backgroundColor: "#f4f4f4",
         color: "#333",
-        lineHeight: "1.6",
       }}
     >
-      <h2>Welcome to My React Page</h2>
-      <p>This content section is styled using inline CSS.</p>
+      <h2>Task 1: Using State in React</h2>
+      <p>Current Count: {count}</p>
+
+      <button
+        onClick={() => setCount(count + 1)}
+        style={{
+          backgroundColor: "navy",
+          color: "white",
+          padding: "10px",
+          borderRadius: "8px",
+          border: "none",
+          cursor: "pointer",
+        }}
+      >
+        Increase
+      </button>
     </main>
   );
 }
